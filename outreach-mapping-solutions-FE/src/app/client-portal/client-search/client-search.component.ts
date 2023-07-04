c79c617f98f3e7c87ff3b660d6add491cd9097b3
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-07-04 15:47:16 -0500

    Completed more initial layout with rough, rough design for client portal and header

--- outreach-mapping-solutions-FE/src/app/client-portal/client-search/client-search.component.ts
+++ outreach-mapping-solutions-FE/src/app/client-portal/client-search/client-search.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-search.component.css']
 })
 export class ClientSearchComponent {
+  monthsDays=[
+    {month: 'January',
+     days: 30},
+     {month: 'February',
+     days: 29},
+     {month: 'March',
+     days: 31},
+     {month: 'April',
+     days: 30},
+     {month: 'May',
+     days: 31},
+     {month: 'June',
+     days: 30},
+     {month: 'July',
+     days: 31},
+     {month: 'August',
+     days: 31},
+     {month: 'September',
+     days: 30},
+     {month: 'October',
+     days: 31},
+     {month: 'November',
+     days: 30},
+     {month: 'December',
+     days: 31},
+  ]
 
+  days: number[]=[];
+  days29=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
+  days30=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+  days31=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+
+  months31Days=["January", "March", "May", "July", "August", "October", "December"];
+  months30Days=["April", "June", "September", "November"];
+
+  monthSelected(event: Event){
+    const monthSelected = (event.target as HTMLSelectElement).value
+    if(this.months31Days.includes(monthSelected)){
+      this.days = this.days31;
+    }
+    if(this.months30Days.includes(monthSelected)){
+      this.days=this.days30;
+    }
+    if(monthSelected==='February'){
+      this.days=this.days29;
+    }
+  }
 }
