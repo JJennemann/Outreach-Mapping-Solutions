94dc8c018f3702d74067ce9abf4a0ba91ba0f9e5
jrolls81 <josh.jennemann@gmail.com>
2023-11-07 11:31:47 -0600

    have very intial map showing in client profile

--- outreach-mapping-solutions-FE/src/app/portals/client-portal/client-profile/client-profile-map/client-profile-map.component.ts
+++ outreach-mapping-solutions-FE/src/app/portals/client-portal/client-profile/client-profile-map/client-profile-map.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
 
 @Component({
   selector: 'app-client-profile-map',
   templateUrl: './client-profile-map.component.html',
   styleUrls: ['./client-profile-map.component.css']
 })
-export class ClientProfileMapComponent {
+export class ClientProfileMapComponent implements OnInit {
+  map: Map;
 
+  ngOnInit(): void {
+    this.map=new Map({
+      target: 'map-container',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0,0],
+        zoom: 2
+      })
+    });
+  }
 }
