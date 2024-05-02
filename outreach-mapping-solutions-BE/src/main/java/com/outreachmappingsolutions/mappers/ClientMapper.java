b1c29937186290cf731e2bfc90bc550383513ff9
jrolls81 <josh.jennemann@gmail.com>
2024-05-02 12:03:22 -0500

    Refactored updateClient endpoint with DTO. Updated mapstruct clientMapper to update client objects, rather than manually setting each field. Also, changed the DTO I was sending back in the createNewClient endpoint. I had originalyl been sending back a DTO with the entire client entity, includign the demographic and contact info subobjects. However, I realized the way I have it set up on the front end, demographics is getting changed at the same time the base is saved (which I may look at changing), so I was worried about the front end getting back a client entity with a demo subobject that didn't match the demo object that was coming back. So, for createNewClient and updateClient, I am only sending back an object of the clientBase as that's the only thing being created or updated and if there is need on the front end for the other objects that will be handle by a call to those specific api endpoints. Also, refactored returnAll endpoint with DTO.

--- outreach-mapping-solutions-BE/src/main/java/com/outreachmappingsolutions/mappers/ClientMapper.java
+++ outreach-mapping-solutions-BE/src/main/java/com/outreachmappingsolutions/mappers/ClientMapper.java
@@ -1,28 +1,36 @@
 package com.outreachmappingsolutions.mappers;
 
 
-import com.outreachmappingsolutions.dtos.ClientBaseDTO;
+import com.outreachmappingsolutions.dtos.CompleteClientEntityDTO;
 import com.outreachmappingsolutions.dtos.ClientContactInfoDTO;
 import com.outreachmappingsolutions.dtos.ClientDemographicsDTO;
+import com.outreachmappingsolutions.dtos.CreateOrUpdateClientBaseDTO;
 import com.outreachmappingsolutions.models.ClientBase;
 import com.outreachmappingsolutions.models.ClientContactInfo;
 import com.outreachmappingsolutions.models.ClientDemographics;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ClientMapper {
 
     ClientMapper INSTANCE = Mappers.getMapper(ClientMapper.class);
 
+    // for returning entire client object, with demo and contact info subobjects
     @Mapping(source = "clientDemographics", target = "clientDemographicsDTO")
     @Mapping(source = "clientContactInfo", target = "clientContactInfoDTO")
-    ClientBaseDTO clientToClientBaseDTO(ClientBase client);
+    CompleteClientEntityDTO clientToClientBaseDTO(ClientBase client);
 
+    // allows for the subobjects to get client ID in DTOs
     @Mapping(source = "client.id", target = "clientId")
     ClientDemographicsDTO demographicsToDemographicsDTO(ClientDemographics clientDemographics);
-
     @Mapping(source = "client.id", target = "clientId")
     ClientContactInfoDTO contactInfoToContactInfoDTO(ClientContactInfo clientContactInfo);
+
+    // allows for updating objects
+    @Mapping (target="clientDemographics", ignore = true)
+    @Mapping (target="clientContactInfo", ignore = true)
+    void updateClientFromDTO(CreateOrUpdateClientBaseDTO dto, @MappingTarget ClientBase entity);
 }
