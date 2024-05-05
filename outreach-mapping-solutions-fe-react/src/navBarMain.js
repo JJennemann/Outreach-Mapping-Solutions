ffcd1599c03232545ae24cffa0072c011cbbf941
jrolls81 <josh.jennemann@gmail.com>
2024-05-05 15:23:49 -0500

    nav bar css adjustments

--- outreach-mapping-solutions-fe-react/src/navBarMain.js
+++ outreach-mapping-solutions-fe-react/src/navBarMain.js
@@ -10,46 +10,41 @@ export function NavBarMain() {
   return (
     <nav className="nav-bar-main">
       <ul className="nav-bar-links">
-        <li>
-          <NavLink
-            to="/client-portal"
-            className={({ isActive }) =>
-              isActive ? "nav-bar-link active" : "nav-bar-link"
-            }
-          >
-            Client Portal
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/report-portal"
-            className={({ isActive }) =>
-              isActive ? "nav-bar-link active" : "nav-bar-link"
-            }
-          >
-            Report Portal
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/map-portal"
-            className={({ isActive }) =>
-              isActive ? "nav-bar-link active" : "nav-bar-link"
-            }
-          >
-            Map Portal
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/log-in"
-            className={({ isActive }) =>
-              isActive ? "nav-bar-link active" : "nav-bar-link"
-            }
-          >
-            Client Portal
-          </NavLink>
-        </li>
+        <NavLink
+          to="/client-portal"
+          className={({ isActive }) =>
+            isActive ? "nav-bar-link active" : "nav-bar-link"
+          }
+        >
+          Client Portal
+        </NavLink>
+
+        <NavLink
+          to="/report-portal"
+          className={({ isActive }) =>
+            isActive ? "nav-bar-link active" : "nav-bar-link"
+          }
+        >
+          Report Portal
+        </NavLink>
+
+        <NavLink
+          to="/map-portal"
+          className={({ isActive }) =>
+            isActive ? "nav-bar-link active" : "nav-bar-link"
+          }
+        >
+          Map Portal
+        </NavLink>
+
+        <NavLink
+          to="/log-in"
+          className={({ isActive }) =>
+            isActive ? "nav-bar-link active" : "nav-bar-link"
+          }
+        >
+          Client Portal
+        </NavLink>
       </ul>
     </nav>
   );
