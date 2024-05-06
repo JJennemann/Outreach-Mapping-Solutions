d8a313b8836f00c5e0816cf33ee4aab4a18311e9
jrolls81 <josh.jennemann@gmail.com>
2024-05-06 10:46:25 -0500

    have initial, very rough searchResults table displaying returned clients from back end. Currently, just have search doing a blind returnAllClients call regardless of whats in the form. Will eventually build in search functionality completing searches based on form data

--- outreach-mapping-solutions-fe-react/src/clientSearchResults/clientSearchResults.js
+++ outreach-mapping-solutions-fe-react/src/clientSearchResults/clientSearchResults.js
@@ -1,3 +1,45 @@
 export function ClientSearchResults({ searchResults }) {
-  return <p>Search Results {console.log(searchResults)}</p>;
+  const searchResultsRows = Object.keys(searchResults).map((resultId) => {
+    const returnedClient = searchResults[resultId];
+    return (
+      <tr key={resultId}>
+        <td>{returnedClient.id}</td>
+        <td>
+          {returnedClient.lastName +
+            ", " +
+            returnedClient.firstName +
+            " " +
+            returnedClient.middleName}
+        </td>
+        <td>
+          {returnedClient.dobMonth +
+            " " +
+            returnedClient.dobDay +
+            ", " +
+            returnedClient.dobYear}
+        </td>
+        <td>
+          {returnedClient.firstThreeSsn +
+            "-" +
+            returnedClient.middleTwoSsn +
+            "-" +
+            returnedClient.lastFourSsn}
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Date of Birth</th>
+          <th>Social Security Number</th>
+        </tr>
+      </thead>
+      <tbody>{searchResultsRows}</tbody>
+    </table>
+  );
 }
