608f807885fe54de3b51f82c42f55ea3c3f32e29
jrolls81 <josh.jennemann@gmail.com>
2024-05-11 04:02:34 -0500

    useEffect hook succesffuly making get call to back end and returning correct client data based on id pulled from query string

--- outreach-mapping-solutions-fe-react/src/pages/clientProfile.jsx
+++ outreach-mapping-solutions-fe-react/src/pages/clientProfile.jsx
@@ -1,11 +1,39 @@
-import { useParams } from "react-router-dom";
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export default function ClientProfile() {
-  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [clientData, setClientData] = useState({});
+  const clientId = searchParams.get("id");
+
+  useEffect(
+    function () {
+      async function fetchClient() {
+        try {
+          const response = await axios.get(
+            `http://localhost:8080/clientBase/return/${clientId}`,
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          console.log(response.data);
+          setClientData(response.data);
+        } catch (error) {
+          console.log("Error:", error);
+        }
+      }
+      fetchClient();
+    },
+    [clientId]
+  );
 
   return (
     <div>
-      <p>Client Profile {id}</p>
+      <p>Client Profile {clientId}</p>
     </div>
   );
 }
